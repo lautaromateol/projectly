59d4a1fc9cbc1f4571cf7e22c4495feaac1cf201
lautaromateol <lautaromateol@gmail.com>
2024-07-11 15:35:23 -0300

    Función para obtener proyectos de la base de datos

---
+++ src/actions/projects/getProjects.js
@@ -0,0 +1,32 @@
+"use server"
+import { auth } from "@/lib/auth.config"
+import { prisma } from "@/lib/prisma"
+
+export async function getProjects() {
+
+  const session = await auth()
+
+  const isAuthenticated = !!session?.user
+
+  if(!isAuthenticated) {
+    return {
+      ok: false,
+      message: "You must be authenticated to get the projects"
+    }
+  }
+
+  try {
+   const projects = await prisma.project.findMany({
+    where: { userId: session.user.id}
+   }) 
+   return {
+    ok: true,
+    projects
+   }
+  } catch (error) {
+    return {
+      ok: false, 
+      message: "Error obtaining projects from database"
+    }
+  }
+}
