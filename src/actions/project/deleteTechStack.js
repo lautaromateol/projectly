c7d9fff8f88f18eb874bde423ce2bb9ea1d670cd
lautaromateol <lautaromateol@gmail.com>
2024-07-23 17:22:35 -0300

    Acción para borrar stack tecnologico

---
+++ src/actions/project/deleteTechStack.js
@@ -0,0 +1,44 @@
+"use server";
+import { auth } from "@/lib/auth.config";
+import { revalidatePath } from "next/cache";
+import prisma from "@/lib/prisma";
+
+export async function deleteTechStack(projectId, id) {
+  try {
+    const session = await auth()
+
+    const isAuthenticated = !!session?.user
+
+    if (!isAuthenticated) {
+      return {
+        ok: false,
+        message: "You must be authenticated to edit an user story"
+      }
+    }
+
+    const project = await prisma.project.findUnique({
+      where: { id: projectId },
+    })
+
+    if (project.userId !== session.user.id) {
+      return {
+        ok: false,
+        message: "You are trying to update a project of another user"
+      }
+    }
+
+    await prisma.techStack.delete({ 
+      where: { id },
+    })
+
+    revalidatePath(`/project/${projectId}`)
+
+    return { ok: true }
+  } catch (error) {
+    console.log(error)
+    return {
+      ok: false,
+      message: "Error deleting the tech stack"
+    }
+  }
+}
