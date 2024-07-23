ee8910e466eade9871a6407ee4167a2d60b3dfd2
lautaromateol <lautaromateol@gmail.com>
2024-07-23 17:22:47 -0300

    Borrado de stack añadido

--- src/app/(app)/project/ui/Stack.jsx
+++ src/app/(app)/project/ui/Stack.jsx
@@ -1,59 +1,62 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { AddOrEditTechStackForm } from "../forms/AddOrEditTechStackForm";
-import { HiOutlinePencil } from "react-icons/hi";
+import { MoveHorizontalIcon, CheckIcon } from "@/icons";
 import { Button } from "@/components/ui/button";
 import { Modal } from "@/components/ui/Modal";
+import { ConfirmDelete } from "@/components/ui/ConfirmDelete";
+import { deleteTechStack } from "@/actions";
 
 export function Stack({ stack, projectId }) {
   return (
-    <Card key={stack.id}>
-      <CardHeader>
-        <div className="flex items-center justify-between">
+    <Modal>
+      <Card className="relative">
+        <CardHeader>
           <CardTitle>{stack.type}</CardTitle>
-          <Modal>
-            <Modal.Opens opens="tech-stack-form">
-            <Button size="sm">
-              <HiOutlinePencil />
-            </Button>
-            </Modal.Opens>
-            <Modal.Window window="tech-stack-form">
-              <AddOrEditTechStackForm stackToEdit={stack} projectId={projectId} />
-            </Modal.Window>
-          </Modal>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <ul className="space-y-2 text-muted-foreground">
-          {
-            stack.tools.map((t, i) => (
-              <li key={i}>
-                <div className="flex items-center gap-2">
-                  <CheckIcon className="w-4 h-4" />
-                  <span>{t}</span>
-                </div>
-              </li>
-            ))
-          }
-        </ul>
-      </CardContent>
-    </Card>
+          <div className="flex items-center gap-2 absolute top-2 right-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <MoveHorizontalIcon className="w-5 h-5" />
+                  <span className="sr-only">Más opciones</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>
+                  <Modal.Opens opens="edit-stack-form">
+                    <button>Edit</button>
+                  </Modal.Opens>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Modal.Opens opens="delete-stack">
+                    <button>Delete</button>
+                  </Modal.Opens>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <ul className="space-y-2 text-muted-foreground">
+            {
+              stack.tools.map((t, i) => (
+                <li key={i}>
+                  <div className="flex items-center gap-2">
+                    <CheckIcon className="w-4 h-4" />
+                    <span>{t}</span>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+        </CardContent>
+      </Card>
+      <Modal.Window window="edit-stack-form">
+        <AddOrEditTechStackForm stackToEdit={stack} projectId={projectId} />
+      </Modal.Window>
+      <Modal.Window window="delete-stack">
+        <ConfirmDelete resourceName="Tech Stack" onClick={() => deleteTechStack(projectId, stack.id)} />
+      </Modal.Window>
+    </Modal>
   )
-}
-
-function CheckIcon(props) {
-  return (
-    (<svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round">
-      <path d="M20 6 9 17l-5-5" />
-    </svg>)
-  );
 }
