caf60840ca5fde511ba7a96e0b152d77b3e329dd
lautaromateol <lautaromateol@gmail.com>
2024-07-23 17:21:28 -0300

    Acción para modificar el estado de una tarea

---
+++ src/actions/project/editTask.js
@@ -0,0 +1,45 @@
+"use server";
+import { auth } from "@/lib/auth.config";
+import { revalidatePath } from "next/cache";
+import prisma from "@/lib/prisma";
+
+export async function editTask(projectId, id, task) {
+  try {
+    const session = await auth()
+
+    const isAuthenticated = !!session?.user
+
+    if (!isAuthenticated) {
+      return {
+        ok: false,
+        message: "You must be authenticated to create an user story"
+      }
+    }
+
+    const project = await prisma.project.findUnique({
+      where: { id: projectId },
+    })
+
+    if (project.userId !== session.user.id) {
+      return {
+        ok: false,
+        message: "You are trying to update a project of another user"
+      }
+    }
+
+    await prisma.task.update({
+      where: { id },
+      data: task
+    })
+
+    revalidatePath(`/project/${projectId}`)
+
+    return { ok: true }
+  } catch (error) {
+    console.log(error)
+    return {
+      ok: false,
+      message: "Error updating the task"
+    }
+  }
+}
