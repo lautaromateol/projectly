d33ebf23295eef78e8705ceb0190d197279cd2ad
lautaromateol <lautaromateol@gmail.com>
2024-08-01 11:20:16 -0300

    Estado de proyecto reemplazado

--- src/app/(app)/projects/ui/Project.jsx
+++ src/app/(app)/projects/ui/Project.jsx
@@ -1,26 +1,29 @@
 "use client";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { CreateOrEditProjectForm } from "./CreateOrEditProjectForm";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { MoveHorizontalIcon } from "@/icons";
+import { ConfirmDelete } from "@/components/ui/ConfirmDelete";
+import { deleteProject } from "@/actions";
 import { formatDate } from "@/lib/utils";
+import { isBefore } from "date-fns";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Modal } from "@/components/ui/Modal";
 import Link from "next/link";
-import { ConfirmDelete } from "@/components/ui/ConfirmDelete";
-import { deleteProject } from "@/actions";
-import { CreateOrEditProjectForm } from "./CreateOrEditProjectForm";
 
-export default function Project({ project }) {
+export function Project({ project }) {
 
-  const status = project.status === "InProgress" ? "In Progress" : "Complete"
+  const time = isBefore(new Date(), new Date(project.dueDate)) ? "On Time" :
+    formatDate(new Date()) === formatDate(new Date(project.dueDate)) ? "Deliver Today" :
+      "Out of Time";
 
   return (
     <Modal>
       <TableRow>
         <TableCell>{project.title}</TableCell>
         <TableCell>
-          <Badge variant="secondary">{status}</Badge>
+          <Badge className={`${time === "On Time" ? "bg-green-500 hover:bg-green-600" : time === "Deliver Today" ? "bg-yellow-500 hover:bg-yellow-600" : "bg-red-500 hover:bg-red-500"} text-white`}>{time}</Badge>
         </TableCell>
         <TableCell>{formatDate(project.startDate)}</TableCell>
         <TableCell>{formatDate(project.dueDate)}</TableCell>
