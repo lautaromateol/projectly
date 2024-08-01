a39fcabedadbee68904ae2a4f9422f40dfaf3896
lautaromateol <lautaromateol@gmail.com>
2024-08-01 11:11:50 -0300

    Archivo de semilla reducido

--- src/lib/seed.js
+++ src/lib/seed.js
@@ -1,37 +1,8 @@
 export const seed = [
   {
-    title: "Project 1",
-    slug: "project-1",
+    title: "Project Management Application",
+    description: "Development of a robust project management application tailored for web developers. The application will streamline the planning, execution, and tracking of projects, offering features such as task management, team collaboration, and analytics. Our goal is to enhance productivity and facilitate efficient project completion by providing an intuitive and comprehensive tool for managing development workflows.",
     startDate: "2024-01-01",
     dueDate: "2024-03-31",
-    status: "InProgress"
   },
-  {
-    title: "Project 2",
-    slug: "project-2",
-    startDate: "2024-02-15",
-    dueDate: "2024-05-15",
-    status: "Complete"
-  },
-  {
-    title: "Project 3",
-    slug: "project-3",
-    startDate: "2024-03-10",
-    dueDate: "2024-06-30",
-    status: "Completed"
-  },
-  {
-    title: "Project 4",
-    slug: "project-4",
-    startDate: "2024-04-01",
-    dueDate: "2024-07-31",
-    status: "InProgress"
-  },
-  {
-    title: "Project 5",
-    slug: "project-5",
-    startDate: "2024-05-15",
-    dueDate: "2024-09-15",
-    status: "Complete"
-  }
 ]
