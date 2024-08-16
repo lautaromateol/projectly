b031584f8470f86ae251695a1c95d0970cd9151a
lautaromateol <lautaromateol@gmail.com>
2024-08-16 12:13:41 -0300

    Función para cerrar sidebar al cambiar de pagina

--- src/components/ui/NavItem.jsx
+++ src/components/ui/NavItem.jsx
@@ -1,8 +1,13 @@
+import { useSidebarContext } from "@/context/SidebarContext"
 import Link from "next/link"
 
 export function NavItem({ title, href, icon }) {
+
+  const { close } = useSidebarContext()
+
   return (
     <Link
+      onClick={close}
       href={href}
       className="flex items-center gap-2 px-3 py-2 rounded-md hover:bg-muted transition-colors"
     >
